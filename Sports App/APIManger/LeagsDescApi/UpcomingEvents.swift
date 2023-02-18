43bd4debd10016fc47dadd7f423ceb016a7c8dc9
zienabhussien <zienabh08@gmail.com>
2023-02-17 16:33:41 -0800

    Upcoming events in Leags Description screen

---
+++ Sports App/APIManger/LeagsDescApi/UpcomingEvents.swift
@@ -0,0 +1,41 @@
+//
+//  UpcomingEvents.swift
+//  Sports App
+//
+//  Created by Zienab on 17/02/2023.
+//
+
+import Foundation
+import Alamofire
+class Event: Decodable{
+    var event_date : String?
+    var event_time : String?
+    var event_first_player : String?
+    var event_second_player : String?
+    var event_first_player_logo : String?
+    var event_second_player_logo : String?
+}
+
+class UpComingEvents: Decodable{
+    
+    var success: Int?
+    var result: [Event]
+    
+    static func fetchCricketLeages(sport:String ,complition : @escaping (UpComingEvents?)-> Void){
+        
+        AF.request("https:apiv2.allsportsapi.com/\(sport)/?met=Fixtures&APIkey=a10943e74d5f6b5225e523a43ddd99c7e3b3678d96a2091c93189206a81c6a34&from=2023-02-17&to=2023-02-17")
+            .responseJSON { res in
+                do
+                {
+                    guard let responseData = res.data else {return}
+                    let res = try JSONDecoder().decode(UpComingEvents.self, from: responseData)
+                    complition(res)
+                    
+                }catch let error {
+                    complition(nil)
+                    print(error.localizedDescription)
+                    
+                }
+            }
+    }
+}
--- Sports App/Views/leaguesdescription/LeaguesDescription.swift
+++ Sports App/Views/leaguesdescription/LeaguesDescription.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-
+import Kingfisher
 class LeaguesDescription: UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
   
+    var upcommingEvents = [Event]()
     var sportType :String?
     
     @IBOutlet weak var upComming: UICollectionView!
@@ -17,10 +18,47 @@ class LeaguesDescription: UIViewController ,UICollectionViewDelegate,UICollectio
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        print("leagueDesc"+(sportType ?? ""))
+    
+       changeSportTypeName()
+        
+        UpComingEvents.fetchCricketLeages(sport: sportType ?? "") { data in
+        
+            if let events = data?.result {
+                self.upcommingEvents = events
+              
+            }
+            
+            DispatchQueue.main.async {
+                self.upComming.reloadData()
+            }
+            
+//            for i in 1..<(data?.result.count ?? 0) {
+//                self.upcommingEvents.append((data?.result[i])!)
+//            }
+        }
+        
+        
         
     }
+    
+    func changeSportTypeName(){
+        switch(sportType){
+        case "Football" :
+            sportType = "football"
+        case "Tennis" :
+            sportType = "tennis"
+        case "Basketball":
+            sportType = "basketball"
+        case "Cricket":
+            sportType = "cricket"
+        default:
+           print("no type")
+        }
+    }
+    
+
+    
+    
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
@@ -29,7 +67,7 @@ class LeaguesDescription: UIViewController ,UICollectionViewDelegate,UICollectio
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if (collectionView == upComming){
             
-            return 20
+            return upcommingEvents.count
             
         }
         if (collectionView == latestResult){
@@ -60,8 +98,14 @@ class LeaguesDescription: UIViewController ,UICollectionViewDelegate,UICollectio
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if (collectionView == upComming){
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "upcoming", for: indexPath) as! UpcomingItem
-            cell.team1Label.text = "team1"
-            cell.team2Label.text = "team2"
+            cell.team1Label.text =  upcommingEvents[indexPath.row].event_first_player
+            print("p1 name : "+(upcommingEvents[indexPath.row].event_time ?? "nill"))
+            cell.team2Label.text = upcommingEvents[indexPath.row].event_second_player
+            cell.team1Img.kf.setImage(with: URL(string:upcommingEvents[indexPath.row].event_first_player_logo ?? ""))
+            print("p1 img : "+(upcommingEvents[indexPath.row].event_first_player_logo ?? ""))
+
+            cell.team2Img.kf.setImage(with: URL(string: upcommingEvents[indexPath.row].event_second_player_logo ?? ""))
+            cell.date.text = upcommingEvents[indexPath.row].event_date
             return cell
             
         }
